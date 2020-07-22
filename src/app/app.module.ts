48e5e88d5ff3658aecd8841e333eee2cda3f1e2d
Nick Kilpatrick <kilpat22@msu.edu>
2020-07-22 17:38:58 -0400

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoremPicsumComponent } from './lorem-picsum/lorem-picsum.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { LoremPicsumComponent } from './lorem-picsum/lorem-picsum.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
